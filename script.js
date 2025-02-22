1fa367f2907be2243c6e8212f3ec87fee14b1211
Abhishek <abhijha283204@gmail.com>
2025-02-22 11:24:32 +0530

    hackathon project under processing

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const apiKey = "688df9ebdad6c382940f9b8b5a2455c4";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBtn = document.querySelector(".search button");
-const searchBox  = document.querySelector(".search box");
-const weatherIcon = document.querySelector(".weather-icon")
+const searchBox  = document.querySelector(".search input");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid = ${apiKey}`)
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+         document.querySelector(".weather").style.display = "none"
+    }
+    
+}
