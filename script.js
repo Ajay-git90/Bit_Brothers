24626063ff014569195296e79d7adf8df50fb551
Abhishek <abhijha283204@gmail.com>
2025-02-22 11:07:54 +0530

    hackathon project under processing

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = "688df9ebdad6c382940f9b8b5a2455c4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBtn = document.querySelector(".search button");
+const searchBox  = document.querySelector(".search box");
+const weatherIcon = document.querySelector(".weather-icon")
